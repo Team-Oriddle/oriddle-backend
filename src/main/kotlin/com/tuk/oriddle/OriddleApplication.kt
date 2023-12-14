1b1ae6b134fcf470c30f02a88fe8f110fe2aed7e
Jmmk <jmk525@gmail.com>
2023-12-15 00:45:57 +0900

    initial commit

---
+++ src/main/kotlin/com/tuk/oriddle/OriddleApplication.kt
@@ -0,0 +1,11 @@
+package com.tuk.oriddle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OriddleApplication
+
+fun main(args: Array<String>) {
+	runApplication<OriddleApplication>(*args)
+}
