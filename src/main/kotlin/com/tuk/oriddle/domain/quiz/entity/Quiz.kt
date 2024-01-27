5f6de6ce186533f50074a1ac21990bdb22f7963f
JeongHunHui <deca3248@gmail.com>
2024-01-27 01:31:01 -0800

    feat(#2): Quiz 엔티티 구현

---
+++ src/main/kotlin/com/tuk/oriddle/domain/quiz/entity/Quiz.kt
@@ -0,0 +1,24 @@
+package com.tuk.oriddle.domain.quiz.entity
+
+import com.tuk.oriddle.global.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+class Quiz(
+    title: String,
+    description: String,
+    image: String
+) : BaseEntity() {
+
+    @Column(nullable = false, length = 100)
+    var title: String = title
+        private set
+
+    @Column(nullable = false, length = 200)
+    var description: String = description
+        private set
+
+    @Column(nullable = false, length = 200)
+    var image: String = image
+        private set
+}
