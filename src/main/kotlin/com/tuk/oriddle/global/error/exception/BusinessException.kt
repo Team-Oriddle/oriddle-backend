1739765f53e07180a7a3784218641eebfd975bc9
JeongHunHui <deca3248@gmail.com>
2024-02-03 16:14:56 -0800

    fix(#12): 다른 자식 예외 클래스가 BusinessException을 상속받을 수 있도록 open 키워드 추가

--- src/main/kotlin/com/tuk/oriddle/global/error/exception/BusinessException.kt
+++ src/main/kotlin/com/tuk/oriddle/global/error/exception/BusinessException.kt
@@ -2,4 +2,4 @@ package com.tuk.oriddle.global.error.exception
 
 import com.tuk.oriddle.global.error.ErrorCode
 
-class BusinessException(val errorCode: ErrorCode) : RuntimeException(errorCode.message)
+open class BusinessException(val errorCode: ErrorCode) : RuntimeException(errorCode.message)
