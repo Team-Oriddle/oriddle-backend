80cc2ec576991c500e129a22df8c2d0bd2e665f0
Jmmk <jmk525@gmail.com>
2024-02-07 13:50:01 +0900

    feat(#14): 퀴즈 페이징 조회 API 구현

--- src/main/kotlin/com/tuk/oriddle/domain/quiz/controller/QuizController.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quiz/controller/QuizController.kt
@@ -1,9 +1,13 @@
 package com.tuk.oriddle.domain.quiz.controller
 
-import com.tuk.oriddle.domain.quiz.dto.response.QuizGetInfo
+import com.tuk.oriddle.domain.quiz.dto.response.GetQuizInfo
+import com.tuk.oriddle.domain.quiz.dto.response.GetQuiz
 import com.tuk.oriddle.domain.quiz.service.query.QuizQueryService
+import com.tuk.oriddle.global.result.ResultCode.QUIZ_PAGING_GET_SUCCESS
 import com.tuk.oriddle.global.result.ResultCode.QUIZ_GET_INFO_SUCCESS
 import com.tuk.oriddle.global.result.ResultResponse
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -14,8 +18,15 @@ class QuizController(private val quizQueryService: QuizQueryService) {
 
     @GetMapping("/info/{quizId}")
     fun getQuizInfo(@PathVariable quizId: Long): ResponseEntity<ResultResponse> {
-        val quizInfo: QuizGetInfo = quizQueryService.getQuizById(quizId)
+        val quizInfo: GetQuizInfo = quizQueryService.getQuizById(quizId)
         return ResponseEntity.ok(ResultResponse.of(QUIZ_GET_INFO_SUCCESS, quizInfo))
     }
 
+    @GetMapping("/page/{page}")
+    fun getQuizzes(@PathVariable page: Int,
+                   @RequestParam(defaultValue = "20") pageSize: Int): ResponseEntity<ResultResponse> {
+        val pageReqeust: PageRequest = PageRequest.of(page, pageSize, Sort.by("id").descending());
+        val quizzes: List<GetQuiz> = quizQueryService.getQuizzesWithPaging(pageReqeust)
+        return ResponseEntity.ok(ResultResponse.of(QUIZ_PAGING_GET_SUCCESS, quizzes))
+    }
 }
