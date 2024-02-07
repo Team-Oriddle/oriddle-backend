2fa7c4b16185c263d151b3d71f447d92ddc15e3a
Jmmk <jmk525@gmail.com>
2024-02-07 13:20:49 +0900

    feat(#14): 퀴즈를 찾을 수 없을 때, 예외 처리 클래스 구현

--- src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
@@ -5,5 +5,8 @@ enum class ErrorCode(val status: Int, val code: String, val message: String) {
     // Global
     INTERNAL_SERVER_ERROR(500, "GL0001", "서버 오류"),
     INPUT_INVALID_VALUE(409, "GL0002", "잘못된 입력"),
-    ;
+
+
+    // Quiz
+    QUIZ_NOT_FOUND(400, "QUIZ001", "퀴즈를 찾을 수 없음");
 }
