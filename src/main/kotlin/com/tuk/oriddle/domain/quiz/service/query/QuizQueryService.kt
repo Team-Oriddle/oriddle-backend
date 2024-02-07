45f0fbb2eeb4925f922613069f680bf3e5283711
Jmmk <jmk525@gmail.com>
2024-02-07 13:49:50 +0900

    feat(#14): 퀴즈 페이징 조회 서비스 로직 구현

--- src/main/kotlin/com/tuk/oriddle/domain/quiz/service/query/QuizQueryService.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quiz/service/query/QuizQueryService.kt
@@ -1,8 +1,11 @@
 package com.tuk.oriddle.domain.quiz.service.query
 
-import com.tuk.oriddle.domain.quiz.dto.response.QuizGetInfo
+import com.tuk.oriddle.domain.quiz.dto.response.GetQuiz
+import com.tuk.oriddle.domain.quiz.dto.response.GetQuizInfo
+import org.springframework.data.domain.PageRequest
 
 interface QuizQueryService
 {
-    fun getQuizById(quizId: Long): QuizGetInfo
+    fun getQuizById(quizId: Long): GetQuizInfo
+    fun getQuizzesWithPaging(pageReqeust: PageRequest): List<GetQuiz>
 }
--- src/main/kotlin/com/tuk/oriddle/domain/quiz/service/query/QuizQueryServiceImpl.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quiz/service/query/QuizQueryServiceImpl.kt
@@ -4,16 +4,20 @@ import com.tuk.oriddle.domain.question.entity.Question
 import com.tuk.oriddle.domain.question.entity.AnswerType
 import com.tuk.oriddle.domain.question.entity.QuestionSourceType
 import com.tuk.oriddle.domain.question.repository.QuestionRepository
-import com.tuk.oriddle.domain.quiz.dto.response.QuizGetInfo
-import com.tuk.oriddle.domain.quiz.dto.response.toQuizGetInfo
+import com.tuk.oriddle.domain.quiz.dto.response.GetQuiz
+import com.tuk.oriddle.domain.quiz.dto.response.GetQuizInfo
+import com.tuk.oriddle.domain.quiz.dto.response.toGetQuizInfo
+import com.tuk.oriddle.domain.quiz.dto.response.toGetQuizzesWithPaging
 import com.tuk.oriddle.domain.quiz.entity.Quiz
 import com.tuk.oriddle.domain.quiz.exception.QuizNotFoundException
 import com.tuk.oriddle.domain.quiz.repository.QuizRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 
 @Service
 class QuizQueryServiceImpl(private val quizRepository: QuizRepository, private val questionRepository: QuestionRepository) : QuizQueryService {
-    override fun getQuizById(quizId: Long): QuizGetInfo {
+    override fun getQuizById(quizId: Long): GetQuizInfo {
         val quiz: Quiz = quizRepository.findById(quizId)
                 .orElseThrow { QuizNotFoundException() }
         val questions: List<Question> = questionRepository.findByQuizId(quizId)
@@ -23,6 +27,11 @@ class QuizQueryServiceImpl(private val quizRepository: QuizRepository, private v
         val answerTypes: List<AnswerType> = questions
                 .map { it.answerType }
                 .distinct()
-        return toQuizGetInfo(quiz,questionSourceTypes,answerTypes)
+        return toGetQuizInfo(quiz,questionSourceTypes,answerTypes)
+    }
+
+    override fun getQuizzesWithPaging(pageReqeust: PageRequest): List<GetQuiz> {
+        val quizPages: Page<Quiz> = quizRepository.findAll(pageReqeust)
+        return toGetQuizzesWithPaging(quizPages)
     }
 }
