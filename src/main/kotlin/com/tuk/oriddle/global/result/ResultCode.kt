28c04596937459f6de07463d293d0b11d1c1bbf5
Jmmk <jmk525@gmail.com>
2024-02-07 13:20:08 +0900

    feat(#14): 퀴즈 정보 조회 성공 결과코드 추가

--- src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
@@ -1,6 +1,9 @@
 package com.tuk.oriddle.global.result
 
+
 enum class ResultCode(val code: String, val message: String) {
     // 도메인 별로 나눠서 관리
+    // Quiz
+    QUIZ_GET_INFO_SUCCESS("QUIZ001", "퀴즈 정보 조회 성공")
     ;
 }
