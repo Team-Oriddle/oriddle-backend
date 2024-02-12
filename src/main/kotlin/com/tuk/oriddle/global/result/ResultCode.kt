7cdfc21f993647a196c770e5513f07fb286fbccf
Jmmk <jmk525@gmail.com>
2024-02-12 13:20:13 +0900

    feat(#13): 퀴즈방 생성 성공 결과 코드 생성

--- src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
@@ -3,5 +3,8 @@ package com.tuk.oriddle.global.result
 enum class ResultCode(val code: String, val message: String) {
     // Quiz (QZ)
     QUIZ_GET_INFO_SUCCESS("QZ0001", "퀴즈 정보 조회 성공"),
-    QUIZ_PAGING_GET_SUCCESS("QZ0002", "퀴즈 페이지 조회 성공")
+    QUIZ_PAGING_GET_SUCCESS("QZ0002", "퀴즈 페이지 조회 성공"),
+
+    // QuizRoom (QZR)
+    QUIZ_ROOM_CREATE_SUCCESS("QZR0001", "퀴즈방 생성 성공")
 }
