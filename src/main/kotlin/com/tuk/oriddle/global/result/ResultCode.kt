ce010bd2d01a4068c395b3590aeaa5247ce4444e
Jmmk <jmk525@gmail.com>
2024-02-19 00:28:50 +0900

    refactor(#13): 결과 코드 2글자로 통일

--- src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
@@ -5,6 +5,6 @@ enum class ResultCode(val code: String, val message: String) {
     QUIZ_GET_INFO_SUCCESS("QZ0001", "퀴즈 정보 조회 성공"),
     QUIZ_PAGING_GET_SUCCESS("QZ0002", "퀴즈 페이지 조회 성공"),
 
-    // QuizRoom (QZR)
-    QUIZ_ROOM_CREATE_SUCCESS("QZR0001", "퀴즈방 생성 성공")
+    // QuizRoom (QR)
+    QUIZ_ROOM_CREATE_SUCCESS("QR0001", "퀴즈방 생성 성공")
 }
