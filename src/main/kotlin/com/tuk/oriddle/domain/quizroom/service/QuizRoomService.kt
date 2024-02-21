c1e46a12e465509ffe12ede735da588a364d8a1e
Jmmk <jmk525@gmail.com>
2024-02-21 22:14:30 +0900

    feat(#20): 퀴즈방 참가 서비스 로직 구현

--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/service/QuizRoomService.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/service/QuizRoomService.kt
@@ -1,17 +1,26 @@
 package com.tuk.oriddle.domain.quizroom.service
 
+import com.tuk.oriddle.domain.participant.entity.Participant
+import com.tuk.oriddle.domain.participant.exception.AlreadyParticipatingException
+import com.tuk.oriddle.domain.participant.service.ParticipantQueryService
 import com.tuk.oriddle.domain.quiz.entity.Quiz
 import com.tuk.oriddle.domain.quiz.service.QuizQueryService
 import com.tuk.oriddle.domain.quizroom.dto.request.QuizRoomCreateRequest
 import com.tuk.oriddle.domain.quizroom.dto.response.QuizRoomCreateResponse
+import com.tuk.oriddle.domain.quizroom.dto.response.QuizRoomJoinResponse
 import com.tuk.oriddle.domain.quizroom.entity.QuizRoom
+import com.tuk.oriddle.domain.quizroom.exception.QuizRoomNotFoundException
 import com.tuk.oriddle.domain.quizroom.repository.QuizRoomRepository
+import com.tuk.oriddle.domain.user.entity.User
+import com.tuk.oriddle.domain.user.service.UserQueryService
 import org.springframework.stereotype.Service
 
 @Service
 class QuizRoomService(
     private val quizRoomRepository: QuizRoomRepository,
-    private val quizQueryService: QuizQueryService
+    private val quizQueryService: QuizQueryService,
+    private val userQueryService: UserQueryService,
+    private val participantQueryService: ParticipantQueryService
 ) {
     fun createQuizRoom(quizRoomCreateRequest: QuizRoomCreateRequest): QuizRoomCreateResponse {
         val quiz: Quiz = quizQueryService.findById(quizRoomCreateRequest.quizId)
@@ -19,4 +28,24 @@ class QuizRoomService(
         quizRoomRepository.save(quizRoom)
         return QuizRoomCreateResponse.of(quizRoom.id)
     }
+
+    fun joinQuizRoom(quizRoomId: Long, userId: Long): QuizRoomJoinResponse {
+        val quizRoom: QuizRoom = quizRoomRepository.findById(quizRoomId)
+            .orElseThrow{ QuizRoomNotFoundException() }
+        val user: User = userQueryService.findById(userId)
+//        checkQuizRoomJoinable(quizRoomId, userId) // 참가할 수 있는 상태인지 검증
+        val participant = Participant(quizRoom, user)
+        participantQueryService.save(participant)
+        return QuizRoomJoinResponse.of(quizRoomId, userId)
+    }
+
+//    private fun checkQuizRoomJoinable(quizRoomId: Long, userId: Long) {
+//        checkUserAlreadyParticipating(quizRoomId, userId)
+//    }
+
+//    private fun checkUserAlreadyParticipating(quizRoomId: Long, userId: Long) {
+//        val participant: Participant? = participantQueryService.findByQuizRoomIdAndUserId(quizRoomId, userId)
+//        if (participant != null)
+//            throw AlreadyParticipatingException()
+//    }
 }
--- src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
@@ -6,5 +6,15 @@ enum class ErrorCode(val status: Int, val code: String, val message: String) {
     INPUT_INVALID_VALUE(409, "GL0002", "잘못된 입력"),
 
     // Quiz (QZ)
-    QUIZ_NOT_FOUND(400, "QZ0001", "퀴즈를 찾을 수 없음");
+    QUIZ_NOT_FOUND(400, "QZ0001", "퀴즈를 찾을 수 없음"),
+
+    // Quiz (QR)
+    QUIZ_ROOM_NOT_FOUND(400, "QR0001", "퀴즈방을 찾을 수 없음"),
+
+    // User (US)
+    USER_NOT_FOUND(400,"US001","유저를 찾을 수 없음"),
+
+    // Participant (PC)
+    PARTICIPANT_NOT_FOUND(400,"PC001","참가자를 찾을 수 없음"),
+    ALREADY_PARTICIPATING(400,"PC001","이미 참여중 입니다.")
 }
