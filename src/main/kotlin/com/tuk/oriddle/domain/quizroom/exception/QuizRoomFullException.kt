801dcba878945e243b3a85820854125f9f95391b
Jmmk <jmk525@gmail.com>
2024-02-22 20:37:03 +0900

    feat(#20): 퀴즈방이 꽉 차있을때, 예외처리 클래스 구현

---
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/exception/QuizRoomFullException.kt
@@ -0,0 +1,8 @@
+package com.tuk.oriddle.domain.quizroom.exception
+
+import com.tuk.oriddle.global.error.ErrorCode
+import com.tuk.oriddle.global.error.exception.BusinessException
+
+class QuizRoomFullException : BusinessException {
+    constructor() : super(ErrorCode.QUIZ_ROOM_IS_FULL)
+}
--- src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
@@ -10,11 +10,11 @@ enum class ErrorCode(val status: Int, val code: String, val message: String) {
 
     // Quiz (QR)
     QUIZ_ROOM_NOT_FOUND(400, "QR0001", "퀴즈방을 찾을 수 없음"),
+    QUIZ_ROOM_IS_FULL(400, "QR002", "퀴즈방이 꽉 차 있음"),
 
     // User (US)
     USER_NOT_FOUND(400,"US001","유저를 찾을 수 없음"),
 
     // Participant (PC)
     PARTICIPANT_NOT_FOUND(400,"PC001","참가자를 찾을 수 없음"),
-    ALREADY_PARTICIPATING(400,"PC001","이미 참여중 입니다.")
 }
