ce76f3bee19d011a21fd9b1458834a39cd8ee79e
JeongHunHui <deca3248@gmail.com>
2024-02-26 01:30:06 +0900

    fix(#23): User 엔티티에서 password 컬럼 삭제

--- src/main/kotlin/com/tuk/oriddle/domain/user/entity/User.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/user/entity/User.kt
@@ -1,20 +1,16 @@
 package com.tuk.oriddle.domain.user.entity
 
 import com.tuk.oriddle.global.entity.BaseEntity
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
 
 @Entity
 class User(
     email: String,
-    password: String,
     nickname: String
 ) : BaseEntity() {
     @Column(name = "email", nullable = false)
     var email: String = email
-        private set
-
-    @Column(name = "password", nullable = false)
-    var password: String = password
         private set
 
     @Column(name = "nickname", nullable = false)
