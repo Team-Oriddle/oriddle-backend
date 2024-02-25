eeb819e7d1ef6e985f7a4a7dec923a8466329276
JeongHunHui <deca3248@gmail.com>
2024-02-26 01:32:34 +0900

    feat(#23): SpringSecurity 설정 파일 구현

---
+++ src/main/kotlin/com/tuk/oriddle/global/config/SecurityConfig.kt
@@ -0,0 +1,33 @@
+package com.tuk.oriddle.global.config
+
+import com.tuk.oriddle.global.oauth.CustomOAuth2UserService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+class SecurityConfig(
+    private val customOAuth2UserService: CustomOAuth2UserService
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.exceptionHandling { exceptions ->
+            exceptions.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        }.logout { logout ->
+            logout.logoutSuccessUrl("/")
+        }.oauth2Login { oauth2Login ->
+            oauth2Login
+                .userInfoEndpoint { userInfoEndpoint ->
+                    userInfoEndpoint.userService(customOAuth2UserService)
+                }
+        }
+        return http.build()
+    }
+}
