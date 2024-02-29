d96bba45c770e7fa5ac16e85963392d3addbc9c0
JeongHunHui <deca3248@gmail.com>
2024-02-29 21:01:04 +0900

    fix(#26): Participant 엔티티에서 quizRoom과 user에 지연로딩이 적용되지 않았던 것을 적용되도록 변경

--- src/main/kotlin/com/tuk/oriddle/domain/participant/entity/Participant.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/participant/entity/Participant.kt
@@ -10,12 +10,12 @@ class Participant(
     quizRoom: QuizRoom,
     user: User
 ) : BaseEntity() {
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "quiz_room_id", nullable = false)
     var quizRoom: QuizRoom = quizRoom
         private set
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     var user: User = user
         private set
