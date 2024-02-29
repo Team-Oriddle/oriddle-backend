4b9264ec945bd0740248b4384df43d4eea706998
JeongHunHui <deca3248@gmail.com>
2024-02-29 15:20:19 +0900

    fix(#13): 퀴즈방을 생성하는 유저 정보를 세션에서 가져오도록 수정

--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/controller/QuizRoomController.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/controller/QuizRoomController.kt
@@ -4,27 +4,26 @@ import com.tuk.oriddle.domain.quizroom.dto.request.QuizRoomCreateRequest
 import com.tuk.oriddle.domain.quizroom.dto.response.QuizRoomCreateResponse
 import com.tuk.oriddle.domain.quizroom.dto.response.QuizRoomJoinResponse
 import com.tuk.oriddle.domain.quizroom.service.QuizRoomService
-import com.tuk.oriddle.global.result.ResultCode.QUIZ_ROOM_JOIN_SUCCESS
 import com.tuk.oriddle.global.result.ResultCode.QUIZ_ROOM_CREATE_SUCCESS
+import com.tuk.oriddle.global.result.ResultCode.QUIZ_ROOM_JOIN_SUCCESS
 import com.tuk.oriddle.global.result.ResultResponse
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.access.annotation.Secured
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/quiz-room")
 class QuizRoomController(private val quizRoomService: QuizRoomService) {
+    @Secured("ROLE_USER")
     @PostMapping
     fun createQuizRoom(
         @Valid @RequestBody request: QuizRoomCreateRequest,
-        @RequestParam(name =  "user-id")
-        userId: Long
+        @AuthenticationPrincipal oauth2User: OAuth2User
     ): ResponseEntity<ResultResponse> {
+        val userId = oauth2User.name.toLong()
         val quizRoom: QuizRoomCreateResponse = quizRoomService.createQuizRoom(request, userId)
         return ResponseEntity.ok(ResultResponse.of(QUIZ_ROOM_CREATE_SUCCESS, quizRoom))
     }
