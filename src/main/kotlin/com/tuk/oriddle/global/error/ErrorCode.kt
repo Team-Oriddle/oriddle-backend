0bdd0d3482c8b6c64e73748f4f0bb96bf1ad6fb0
JeongHunHui <deca3248@gmail.com>
2024-02-29 16:42:19 +0900

    fix(#13): 에러 코드가 6자리가 아닌 부분 수정

--- src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/error/ErrorCode.kt
@@ -11,11 +11,11 @@ enum class ErrorCode(val status: Int, val code: String, val message: String) {
 
     // Quiz (QR)
     QUIZ_ROOM_NOT_FOUND(400, "QR0001", "퀴즈방을 찾을 수 없음"),
-    QUIZ_ROOM_IS_FULL(400, "QR002", "퀴즈방이 꽉 차 있음"),
+    QUIZ_ROOM_IS_FULL(400, "QR0002", "퀴즈방이 꽉 차 있음"),
 
     // User (US)
-    USER_NOT_FOUND(400,"US001","유저를 찾을 수 없음"),
+    USER_NOT_FOUND(400,"US0001","유저를 찾을 수 없음"),
 
     // Participant (PC)
-    PARTICIPANT_NOT_FOUND(400,"PC001","참가자를 찾을 수 없음"),
+    PARTICIPANT_NOT_FOUND(400,"PC0001","참가자를 찾을 수 없음"),
 }
