5147738c71586aa4661cc76dbea6b6c1308a2e69
JeongHunHui <deca3248@gmail.com>
2024-02-29 16:46:25 +0900

    feat(#13): 필요없는 메서드 제거 및 유저가 퀴즈방에 참여중인지 판단하는 메서드 추가

--- src/main/kotlin/com/tuk/oriddle/domain/participant/service/ParticipantQueryService.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/participant/service/ParticipantQueryService.kt
@@ -1,22 +1,16 @@
 package com.tuk.oriddle.domain.participant.service
 
 import com.tuk.oriddle.domain.participant.entity.Participant
-import com.tuk.oriddle.domain.participant.exception.ParticipantNotFoundException
 import com.tuk.oriddle.domain.participant.repository.ParticipantRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ParticipantQueryService(private val participantRepository: ParticipantRepository) {
-    fun findByQuizRoomIdAndUserId(quizRoomId: Long, userId: Long): Participant {
-        return participantRepository.findByQuizRoomIdAndUserId(quizRoomId, userId)
-            .orElseThrow{ ParticipantNotFoundException() }
-    }
-
-    fun countParticipantsByQuizRoomId(quizRoomId: Long): Int {
-        return participantRepository.countParticipantsByQuizId(quizRoomId)
-    }
-
     fun save(participant: Participant) {
         participantRepository.save(participant)
+    }
+
+    fun isUserAlreadyParticipant(quizRoomId: Long, userId: Long): Boolean {
+        return participantRepository.existsByQuizRoomIdAndUserId(quizRoomId, userId)
     }
 }
