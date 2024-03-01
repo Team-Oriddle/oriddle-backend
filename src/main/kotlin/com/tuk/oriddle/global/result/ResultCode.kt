ac41b9e140e30ae8504d649667354e2c825838c3
Jmmk <jmk525@gmail.com>
2024-03-01 21:43:33 +0900

    feat(#25): 퀴즈방 정보 조회 API 구현

--- src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
+++ src/main/kotlin/com/tuk/oriddle/global/result/ResultCode.kt
@@ -9,6 +9,7 @@ enum class ResultCode(val code: String, val message: String) {
     QUIZ_ROOM_CREATE_SUCCESS("QR0001", "퀴즈방 생성 성공"),
     QUIZ_ROOM_JOIN_SUCCESS("QR0002", "퀴즈방 참여 성공"),
     QUIZ_ROOM_LEAVE_SUCCESS("QR0003", "퀴즈방 퇴장 성공"),
+    QUIZ_ROOM_GET_INFO_SUCCESS("QR0004","퀴즈방 정보 조회 성공"),
 
     // User (US)
     USER_GET_SUCCESS("US0001", "유저 정보 조회 성공"),
