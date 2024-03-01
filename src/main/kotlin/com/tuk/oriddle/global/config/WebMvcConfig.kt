577a102c6dbe088d7c7dd13d6bab31927f60557e
JeongHunHui <deca3248@gmail.com>
2024-03-01 23:15:35 +0900

    hotFix(#develop): allowCredentials 제거

--- src/main/kotlin/com/tuk/oriddle/global/config/WebMvcConfig.kt
+++ src/main/kotlin/com/tuk/oriddle/global/config/WebMvcConfig.kt
@@ -11,6 +11,5 @@ class WebMvcConfig : WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("*")
             .allowedHeaders("*")
-            .allowCredentials(true)
     }
 }
