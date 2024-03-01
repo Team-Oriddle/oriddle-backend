774cc34d621dc88c9c14dbcdeed79a568d1b0f81
Jmmk <jmk525@gmail.com>
2024-03-01 23:02:35 +0900

    refactor(#25): 퀴즈방 정보 조회 응답 DTO의 필드 users -> participators로 변경

--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/dto/response/QuizRoomGetInfoResponse.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/dto/response/QuizRoomGetInfoResponse.kt
@@ -7,18 +7,18 @@ data class QuizRoomGetInfoResponse(
     val roomTitle: String,
     val quizTitle: String,
     val maxParticipant: Int,
-    val users: List<ParticipantGetInfoResponse>
+    val participators: List<ParticipantGetInfoResponse>
 ) {
     companion object {
         fun of(
             quizRoom: QuizRoom,
-            users: List<ParticipantGetInfoResponse>
+            participators: List<ParticipantGetInfoResponse>
         ): QuizRoomGetInfoResponse {
             return QuizRoomGetInfoResponse(
                 roomTitle = quizRoom.title,
                 quizTitle = quizRoom.quiz.title,
                 maxParticipant = quizRoom.maxParticipant,
-                users = users
+                participators = participators
             )
         }
     }
