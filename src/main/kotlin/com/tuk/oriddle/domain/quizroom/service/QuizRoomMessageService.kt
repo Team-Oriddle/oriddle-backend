31c978eb478487d0668b29ac1629aecd1b030082
JeongHunHui <deca3248@gmail.com>
2024-03-08 05:26:31 +0900

    feat(#32): 퀴즈 시작 메시지를 보내는 기능 구현

--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/service/QuizRoomMessageService.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/service/QuizRoomMessageService.kt
@@ -3,12 +3,16 @@ package com.tuk.oriddle.domain.quizroom.service
 import com.tuk.oriddle.domain.quizroom.dto.message.JoinQuizRoomMessage
 import com.tuk.oriddle.domain.quizroom.dto.message.LeaveQuizRoomMessage
 import com.tuk.oriddle.domain.quizroom.dto.message.QuestionMessage
+import com.tuk.oriddle.domain.quizroom.dto.message.StartQuizRoomMessage
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class QuizRoomMessageService(
-    private val messagingTemplate: SimpMessagingTemplate
+    private val messagingTemplate: SimpMessagingTemplate,
+    @Value("\${config.quiz-room.start-wait-time}")
+    private val startWaitTime: Long
 ) {
     fun sendQuizRoomJoinMessage(quizRoomId: Long, userId: Long, nickname: String, position: Int) {
         messagingTemplate.convertAndSend(
@@ -21,6 +25,13 @@ class QuizRoomMessageService(
         messagingTemplate.convertAndSend(
             "/topic/quiz-room/$quizRoomId/leave",
             LeaveQuizRoomMessage(userId)
+        )
+    }
+
+    fun sendQuizRoomStartMessage(quizRoomId: Long) {
+        messagingTemplate.convertAndSend(
+            "/topic/quiz-room/$quizRoomId/start",
+            StartQuizRoomMessage(startWaitTime)
         )
     }
 
