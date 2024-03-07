7a1fc001ee3d2c1db6b0081a2a456b6299fff67d
JeongHunHui <deca3248@gmail.com>
2024-03-08 05:14:24 +0900

    chore(#32): 스케줄링 활성화 하도록 설정

--- src/main/kotlin/com/tuk/oriddle/OriddleApplication.kt
+++ src/main/kotlin/com/tuk/oriddle/OriddleApplication.kt
@@ -3,9 +3,11 @@ package com.tuk.oriddle
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 class OriddleApplication
 
 fun main(args: Array<String>) {
