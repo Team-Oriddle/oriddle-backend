d7bfdab4b5fe91e6dbcaabdb1fc169928eeb5eaf
JeongHunHui <deca3248@gmail.com>
2024-03-11 23:19:26 +0900

    feat(#37): 클라이언트로 부터 정답 확인 메시지를 받는 controller 구현 및 정답 확인 기능 구현

---
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/controller/QuizRoomWsController.kt
@@ -0,0 +1,24 @@
+package com.tuk.oriddle.domain.quizroom.controller
+
+import com.tuk.oriddle.domain.quizroom.dto.message.CheckAnswerMessage
+import com.tuk.oriddle.domain.quizroom.service.QuizRoomService
+import com.tuk.oriddle.global.result.ResultCode.*
+import org.springframework.messaging.handler.annotation.DestinationVariable
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
+
+@Controller
+class QuizRoomWsController(private val quizRoomService: QuizRoomService) {
+    @MessageMapping("/quiz-room/{quizRoomId}/check-answer")
+    fun handleMessage(
+        @DestinationVariable("quizRoomId") quizRoomId: Long,
+        @Payload message: CheckAnswerMessage,
+        headerAccessor: SimpMessageHeaderAccessor
+    ) {
+        val userId = (headerAccessor.sessionAttributes?.get("id") as String).toLong()
+        quizRoomService.checkAnswer(quizRoomId, message, userId)
+    }
+}
