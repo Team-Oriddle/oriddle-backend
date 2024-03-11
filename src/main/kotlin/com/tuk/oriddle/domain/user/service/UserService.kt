d6272c8db3d25c4e674f4292718b79eca4a98ae1
Jmmk <jmk525@gmail.com>
2024-03-11 19:04:21 +0900

    feat(#38): 유저 닉네임 업데이트 서비스 로직 구현

--- src/main/kotlin/com/tuk/oriddle/domain/user/service/UserService.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/user/service/UserService.kt
@@ -1,7 +1,11 @@
 package com.tuk.oriddle.domain.user.service
 
+import com.tuk.oriddle.domain.user.dto.request.UserNicknameUpdateRequest
+import com.tuk.oriddle.domain.user.dto.response.UserNicknameUpdateResponse
 import com.tuk.oriddle.domain.user.entity.Modifier
 import com.tuk.oriddle.domain.user.entity.User
+import jakarta.transaction.Transactional
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
 import kotlin.math.pow
 
@@ -12,6 +16,14 @@ class UserService(private val userQueryService: UserQueryService) {
     fun createByEmail(email: String): User {
         val user = User(email = email, nickname = generateRandomNickname(characterName, randomDigit))
         return userQueryService.save(user)
+    }
+
+    @Transactional
+    fun updateNickname(oauth2User: OAuth2User, request: UserNicknameUpdateRequest): UserNicknameUpdateResponse {
+        val userId = oauth2User.attributes["userId"] as Long
+        val user = userQueryService.findById(userId)
+        user.updateNickname(request.getUpdatedNickname())
+        return UserNicknameUpdateResponse.of(user)
     }
 
     private fun generateRandomNickname(duck: String, numberLength: Int): String {
