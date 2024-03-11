0c68125773e551c0f5f3b9cdbfa6a4e830444aa4
JeongHunHui <deca3248@gmail.com>
2024-03-11 23:10:05 +0900

    refactor(#37): 퀴즈방 상태 조회 시 Repository를 사용하도록 리팩토링

--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/exception/QuizRoomStatusNotFoundInRedisException.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/exception/QuizRoomStatusNotFoundInRedisException.kt
@@ -3,7 +3,6 @@ package com.tuk.oriddle.domain.quizroom.exception
 import com.tuk.oriddle.global.error.ErrorCode
 import com.tuk.oriddle.global.error.exception.BusinessException
 
-// TODO: 패키지 바꾸기
-class QuizStatusNotFoundInRedisException : BusinessException {
-    constructor() : super(ErrorCode.QUIZ_STATUS_NOT_FOUND_IN_REDIS)
+class QuizRoomStatusNotFoundInRedisException : BusinessException {
+    constructor() : super(ErrorCode.QUIZ_ROOM_STATUS_NOT_FOUND_IN_REDIS)
 }
--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/service/QuizRoomRedisService.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/service/QuizRoomRedisService.kt
@@ -1,68 +1,21 @@
 package com.tuk.oriddle.domain.quizroom.service
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.tuk.oriddle.domain.participant.entity.Participant
-import com.tuk.oriddle.domain.question.entity.Question
-import com.tuk.oriddle.domain.quizroom.dto.redis.QuestionRedisDto
-import com.tuk.oriddle.domain.quizroom.dto.redis.QuizStatusRedisDto
-import com.tuk.oriddle.domain.quizroom.dto.redis.UserRedisDto
-import com.tuk.oriddle.domain.quizroom.exception.QuestionNotFoundInRedisException
-import com.tuk.oriddle.domain.quizroom.exception.QuizStatusNotFoundInRedisException
-import org.springframework.data.redis.core.RedisTemplate
+import com.tuk.oriddle.domain.quizroom.dto.redis.QuizRoomStatusRedisDto
+import com.tuk.oriddle.domain.quizroom.exception.QuizRoomStatusNotFoundInRedisException
+import com.tuk.oriddle.domain.quizroom.repository.QuizRoomStatusRedisRepository
 import org.springframework.stereotype.Service
 
-// TODO: Redis Repository 계층을 쓰도록 리팩토링
 @Service
 class QuizRoomRedisService(
-    private val redisTemplate: RedisTemplate<String, Any>,
-    private val objectMapper: ObjectMapper
+    private val quizRoomStatusRedisRepository: QuizRoomStatusRedisRepository
 ) {
-    fun saveQuizStatus(quizRoomId: Long, quizId: Long, questionCount: Long) {
-        val key = "qr:$quizRoomId:status"
-        val status = QuizStatusRedisDto(quizId, questionCount)
-        redisTemplate.opsForValue().set(key, status)
+    fun saveQuizStatus(quizRoomId: Long, questionCount: Long) {
+        val status = QuizRoomStatusRedisDto(quizRoomId, questionCount)
+        quizRoomStatusRedisRepository.save(status)
     }
 
-    fun getQuizStatus(quizRoomId: Long): QuizStatusRedisDto {
-        val key = "qr:$quizRoomId:status"
-        val value =
-            redisTemplate.opsForValue().get(key) ?: throw QuizStatusNotFoundInRedisException()
-        return objectMapper.convertValue(value, QuizStatusRedisDto::class.java)
-    }
-
-    fun saveQuestionsAndAnswers(quizRoomId: Long, questions: MutableList<Question>) {
-        questions.map {
-            saveQuestion(quizRoomId, it.number, it)
-            saveAnswer(quizRoomId, it.number, it.getAnswerSet())
-        }
-    }
-
-    private fun saveQuestion(quizRoomId: Long, number: Long, question: Question) {
-        val key = "qr:$quizRoomId:question:$number"
-        val questionRedisDto = QuestionRedisDto.of(question)
-        redisTemplate.opsForValue().set(key, questionRedisDto)
-    }
-
-    private fun saveAnswer(quizRoomId: Long, number: Long, answers: MutableSet<String>) {
-        val key = "qr:$quizRoomId:answer:$number"
-        redisTemplate.opsForValue().set(key, answers)
-    }
-
-    fun getQuestion(quizRoomId: Long, number: Long): QuestionRedisDto {
-        val key = "qr:$quizRoomId:question:$number"
-        val value = redisTemplate.opsForValue().get(key) ?: throw QuestionNotFoundInRedisException()
-        return objectMapper.convertValue(value, QuestionRedisDto::class.java)
-    }
-
-    fun saveQuizParticipants(quizRoomId: Long, participants: MutableList<Participant>) {
-        val key = "qr:$quizRoomId:users"
-        val participantInfos = makeParticipantInfosByParticipants(participants)
-        redisTemplate.opsForValue().set(key, participantInfos)
-    }
-
-    private fun makeParticipantInfosByParticipants(participants: MutableList<Participant>): MutableList<UserRedisDto> {
-        return participants.map {
-            UserRedisDto(it.user.id, it.position)
-        } as MutableList<UserRedisDto>
+    fun getQuizStatus(quizRoomId: Long): QuizRoomStatusRedisDto {
+        return quizRoomStatusRedisRepository.findById(quizRoomId)
+            .orElseThrow { QuizRoomStatusNotFoundInRedisException() }
     }
 }
