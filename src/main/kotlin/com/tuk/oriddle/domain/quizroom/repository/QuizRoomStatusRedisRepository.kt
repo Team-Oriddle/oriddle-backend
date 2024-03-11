b973b4103ee13962ededaecff63eb409edf06c00
JeongHunHui <deca3248@gmail.com>
2024-03-12 02:35:17 +0900

    fix(#41): 퀴즈방 상태 저장 객체 변경에 따라 Service와 Repository 변경

--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/repository/QuizRoomStatusRedisRepository.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/repository/QuizRoomStatusRedisRepository.kt
@@ -1,7 +1,7 @@
 package com.tuk.oriddle.domain.quizroom.repository
 
-import com.tuk.oriddle.domain.quizroom.dto.redis.QuizRoomStatusRedisDto
+import com.tuk.oriddle.domain.quizroom.entity.QuizRoomProgressStatus
 import org.springframework.data.repository.CrudRepository
 
-interface QuizRoomStatusRedisRepository : CrudRepository<QuizRoomStatusRedisDto, Long> {
+interface QuizRoomStatusRedisRepository : CrudRepository<QuizRoomProgressStatus, Long> {
 }
