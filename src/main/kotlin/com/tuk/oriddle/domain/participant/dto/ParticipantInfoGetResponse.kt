7f258cc2bfe375295e072d68a1a9692b3950fdb1
Jmmk <jmk525@gmail.com>
2024-03-12 19:30:53 +0900

    refactor(#42): 퀴즈방 정보 조회 응답 DTO에 참가자의 방장 여부 추가

--- src/main/kotlin/com/tuk/oriddle/domain/participant/dto/ParticipantInfoGetResponse.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/participant/dto/ParticipantInfoGetResponse.kt
@@ -1,11 +1,13 @@
 package com.tuk.oriddle.domain.participant.dto
 
 import com.tuk.oriddle.domain.participant.entity.Participant
+import com.tuk.oriddle.domain.participant.entity.Role
 
 data class ParticipantInfoGetResponse(
     val userId: Long,
     val position: Int,
-    val nickname: String
+    val nickname: String,
+    val role: Role
 ) {
     companion object {
         fun of(
@@ -14,7 +16,8 @@ data class ParticipantInfoGetResponse(
             return ParticipantInfoGetResponse(
                 userId =  participant.user.id,
                 position = participant.position,
-                nickname = participant.user.nickname
+                nickname = participant.user.nickname,
+                role = participant.role
             )
         }
     }
