34256b3ad3c71d0e089a89834f38ce8cc16adbb8
JeongHunHui <deca3248@gmail.com>
2024-03-18 15:28:31 +0900

    feat(#50): QuizRoomCreateRequest 유효성 검증 어노테이션 추가 및 수정

--- src/main/kotlin/com/tuk/oriddle/domain/quizroom/dto/request/QuizRoomCreateRequest.kt
+++ src/main/kotlin/com/tuk/oriddle/domain/quizroom/dto/request/QuizRoomCreateRequest.kt
@@ -3,13 +3,15 @@ package com.tuk.oriddle.domain.quizroom.dto.request
 import com.tuk.oriddle.domain.quiz.entity.Quiz
 import com.tuk.oriddle.domain.quizroom.entity.QuizRoom
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
+import org.hibernate.validator.constraints.Length
+import org.hibernate.validator.constraints.Range
 
-data class QuizRoomCreateRequest(
+data class QuizRoomCreateRequest constructor(
     val quizId: Long,
-    @field:NotBlank(message = "title은 비어 있을 수 없습니다.")
+    @field:NotBlank
+    @field:Length(max = 20)
     val title: String,
-    @field:NotNull(message = "maxParticipant는 비어 있을 수 없습니다.")
+    @field:Range(min = 2, max = 8)
     val maxParticipant: Int,
 ) {
     fun toEntity(quiz: Quiz): QuizRoom {
